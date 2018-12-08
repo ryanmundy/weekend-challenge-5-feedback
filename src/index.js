abdb6012054cd965fbad0ee4060c9d38a9bba2ed
Ryan Mundy <ryanmundy34@gmail.com>
2018-12-08 11:14:04 -0600

    set up file structure

--- src/index.js
+++ src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const stepOneReducer = (state = [], action) => {
+    
+    return state
+}
+
+const stepTwoReducer = (state = [], action) => {
+
+    return state
+}
+
+const stepThreeReducer = (state = [], action) => {
+
+    return state
+}
+
+const stepFourReducer = (state = [], action) => {
+
+    return state
+}
+
+const storeInstance = createStore(
+    combineReducers({
+        stepOneReducer,
+        stepTwoReducer,
+        stepThreeReducer,
+        stepFourReducer
+    }),
+    applyMiddleware(logger)
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
